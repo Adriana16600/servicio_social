b3199acd9c48cdb17b9f2e45f0d5cf266385c7d6
David Hernandez <davidh.c001606@gmil.com>
2021-11-17 21:07:18 -0700

    chingos de cosas

--- lib/src/excel.dart
+++ lib/src/excel.dart
@@ -1,229 +1,69 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:excel/excel.dart';
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:ss_admin/src/modules/navigation/navigation_model.dart';
+import 'package:flutter/cupertino.dart';
 
-class ExportPage extends StatefulWidget {
-  @override
-  _ExportPageState createState() => _ExportPageState();
-}
+class Export {
+  exportDevices(BuildContext context) async {
+    Excel excel = Excel.createExcel();
+    Sheet sheetObject;
+    sheetObject = excel['Sheet1'];
+    List<String> abcs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    var titles = [
+      'Nombre', //a
+      'Departamento', //b
+      'Numero', //c
+      'Marca', //d
+      'Disco', //e
+      'RAM', //f
+      'Modelo', //g
+      'Procesador', //h
+    ];
+    for (int i = 0; i < titles.length; i++)
+      sheetObject.cell(CellIndex.indexByString("${abcs[i]}1")).value =
+      titles[i];
 
-class _ExportPageState extends State<ExportPage> {
-  bool done = false;
-  var excel = Excel.createExcel();
-  List<dynamic> elements = [];
-  Sheet sheetObject;
+    List<DocumentSnapshot> departments = await FirebaseFirestore.instance
+        .collection('departments')
+        .get()
+        .then((value) => value.docs);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Exportar a EXCEL', style: Theme.of(context).textTheme.headline5),
-            Icon(
-              Icons.cloud_download_rounded,
-              size: 150,
-              color: Theme.of(context).colorScheme.primary.withOpacity(.5),
-            ),
-            Text('Este proceso puede tomar de 2-3 minutos',
-                style: Theme.of(context).textTheme.bodyText2),
-            Container(
-              width: 500,
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: ElevatedButton.icon(
-                  onPressed: !done
-                      ? () {
-                    showLoaderDialog(context);
-                    print('Working...');
-                    sheetObject = excel['Sheet1'];
-                    var abcs = [
-                      'a',
-                      'b',
-                      'c',
-                      'd',
-                      'e',
-                      'f',
-                      'g',
-                      'h',
-                      'i',
-                      'j',
-                      'k',
-                      'l',
-                      'm',
-                      'n',
-                      'o',
-                      'p',
-                      'q',
-                      'r',
-                      's',
-                      't',
-                      'u',
-                      'v',
-                      'w',
-                      'x',
-                      'y',
-                      'z'
-                    ];
-                    var titles = [
-                      'folio',
-                      'nombre del participante',
-                      'contacto',
-                      'ciudad',
-                      'entidad',
-                      'estado boleto',
-                      'metodo de pago',
-                      'numero tarjeta',
-                      'fecha apartado',
-                      'fecha venta',
-                    ];
-                    for (int i = 0; i < titles.length; i++) {
-                      sheetObject
-                          .cell(CellIndex.indexByString("${abcs[i]}1"))
-                          .value = titles[i];
-                    }
+    List<DocumentSnapshot> devices = await FirebaseFirestore.instance
+        .collection('devices')
+        .where('specs_added', isEqualTo: true)
+        .get()
+        .then((value) => value.docs)
+        .onError((error, stackTrace) {
+      print('${error.code}');
+      return [];
+    });
 
-                    FirebaseFirestore.instance
-                        .collection('raffles')
-                        .doc(Provider.of<NavigationModel>(context, listen: false).activeRaffle)
-                        .collection('tickets')
-                        .where('number', isLessThanOrEqualTo: 9999)
-                    // .orderBy('number', descending: false)
-                        .get()
-                        .then((value) {
-                      elements = value.docs;
-                      print('done!!!!');
-                      print('Items count: ${value.docs.length}');
-                      setState(() {
-                        done = true;
-                      });
-                      Navigator.pop(context);
-                    });
-                  }
-                      : null,
-                  icon: Icon(Icons.cloud_done_rounded),
-                  label: Text('Preparar información')),
-            ),
-            Container(
-              width: 500,
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: ElevatedButton.icon(
-                  onPressed: done
-                      ? () {
-                    showLoaderDialog(context);
-                    int actualTicketsIndex = 0;
-                    for (int i = 0; i < elements.length; i++) {
-                      if (actualTicketsIndex < 10000 - 1) {
-                        if (elements[actualTicketsIndex]['number'] == i) {
-                          addToSheet(elements: elements[i], index: i);
-                          actualTicketsIndex++;
-                          if (elements[actualTicketsIndex]['number'] ==
-                              i) {
-                            i--;
-                          }
-                        } else {
-                          addToSheetEmpty(index: i);
-                        }
-                      } else {
-                        addToSheetEmpty(index: i);
-                      }
-                      print(i);
-                    }
-                    excel.save(
-                        fileName:
-                        'boletos ${DateFormat('dd-MMMM-yyyy H m', 'es-MX').format(DateTime.now())}.xlsx');
-                    setState(() {
-                      done = false;
-                    });
-                    Navigator.pop(context);
-                  }
-                      : null,
-                  icon: Icon(Icons.arrow_circle_down_rounded),
-                  label: Text('Generar y descargar Excel')),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+    for (int i = 0; i < devices.length; i++) {
+      sheetObject.cell(CellIndex.indexByString("A${i + 2}")).value =
+      devices[i]['name'];
+      for (var j = 0; j < departments.length; ++j) {
+        if (departments[j].id == devices[i]['department']) {
+          sheetObject.cell(CellIndex.indexByString("B${i + 2}")).value =
+          '${departments[j]['name']} - ${departments[j]['building']} - ${departments[j]['prefix']}';
+        }
+      }
 
-  showLoaderDialog(BuildContext context) {
-    AlertDialog alert = AlertDialog(
-      content: new Row(
-        children: [
-          CircularProgressIndicator(),
-          Container(
-              margin: EdgeInsets.only(left: 7), child: Text("Cargando...")),
-        ],
-      ),
-    );
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
-
-  void addToSheet({DocumentSnapshot elements, int index}) {
-    sheetObject.cell(CellIndex.indexByString("A${index + 2}")).value =
-        num(elements.data()['number']);
-    sheetObject.cell(CellIndex.indexByString("B${index + 2}")).value =
-    elements.data()['owner']['name'];
-    sheetObject.cell(CellIndex.indexByString("C${index + 2}")).value =
-    elements.data()['owner']['phone_number'];
-    sheetObject.cell(CellIndex.indexByString("D${index + 2}")).value =
-    elements.data()['owner']['city'];
-    sheetObject.cell(CellIndex.indexByString("E${index + 2}")).value =
-    elements.data()['owner']['state'];
-    if (elements.data()['sold_time'] != null) {
-      sheetObject.cell(CellIndex.indexByString("F${index + 2}")).value =
-      'VENIDO';
-    } else {
-      sheetObject.cell(CellIndex.indexByString("F${index + 2}")).value =
-      'APARTADO';
+      sheetObject.cell(CellIndex.indexByString("C${i + 2}")).value =
+      devices[i]['number'];
+      sheetObject.cell(CellIndex.indexByString("D${i + 2}")).value =
+      devices[i]['specs.brand'];
+      sheetObject.cell(CellIndex.indexByString("E${i + 2}")).value =
+      devices[i]['specs.memory'];
+      sheetObject.cell(CellIndex.indexByString("F${i + 2}")).value =
+      devices[i]['specs.ram'];
+      sheetObject.cell(CellIndex.indexByString("G${i + 2}")).value =
+      devices[i]['specs.model'];
+      sheetObject.cell(CellIndex.indexByString("H${i + 2}")).value =
+      devices[i]['specs.processor'];
     }
 
-    if (elements.data()['payment'] != null) {
-      sheetObject.cell(CellIndex.indexByString("g${index + 2}")).value =
-      elements.data()['payment']['type'];
-      //card_4digits
-      sheetObject.cell(CellIndex.indexByString("h${index + 2}")).value =
-      elements.data()['payment']['card_4digits'];
-    }
-    sheetObject.cell(CellIndex.indexByString("i${index + 2}")).value =
-        DateFormat('dd MMMM yyyy, H:m', 'es-MX')
-            .format(elements.data()['apart_time'].toDate());
-    if (elements.data()['sold_time'] != null) {
-      sheetObject.cell(CellIndex.indexByString("j${index + 2}")).value =
-          DateFormat('dd MMMM yyyy, H:m', 'es-MX')
-              .format(elements.data()['sold_time'].toDate());
-    }
-  }
-
-  void addToSheetEmpty({int index}) {
-    sheetObject.cell(CellIndex.indexByString("A${index + 2}")).value =
-        num(index);
-    sheetObject.cell(CellIndex.indexByString("B${index + 2}")).value = '';
-    sheetObject.cell(CellIndex.indexByString("C${index + 2}")).value = '';
-    sheetObject.cell(CellIndex.indexByString("D${index + 2}")).value = '';
-    sheetObject.cell(CellIndex.indexByString("E${index + 2}")).value = '';
-    sheetObject.cell(CellIndex.indexByString("F${index + 2}")).value =
-    'DISPONIBLE';
-    sheetObject.cell(CellIndex.indexByString("i${index + 2}")).value = '';
-  }
-
-  String num(int index) {
-    return index < 10
-        ? '000$index'
-        : index < 100
-        ? '00$index'
-        : index < 1000
-        ? '0$index'
-        : '$index';
+    excel.save(
+        fileName:
+        'reporte.xlsx');
+    Navigator.pop(context);
   }
 }
