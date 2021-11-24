9d2a04cf9bc6a95b6bbc0e055e1820b712abd5bb
HP <yuliezmz@gmail.com>
2021-11-24 03:12:34 -0700

    1yyy

--- lib/src/generar_reportes.dart
+++ lib/src/generar_reportes.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:servicio_social/src/admi_page.dart';
 import 'package:servicio_social/src/excel.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -15,6 +16,81 @@ class ReportesPage extends StatefulWidget {
 class _ReportesPageState extends State<ReportesPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Generar reportes'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TablaAlumnos(),
+                  ));
+            },
+            icon: Icon(
+              Icons.history_rounded,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: HugeButton(
+            color: Theme.of(context).colorScheme.primary,
+            icon: Icons.import_contacts,
+            text: 'Carta de aceptación',
+            onTap: () {}
+        ),
+        child: HugeButton(
+            color: Theme.of(context).colorScheme.primary,
+            icon: Icons.menu_book,
+            text: 'Carta de terminación',
+            onTap: () {}
+        ),
+      ),
+    );
+  }
+}
+
+class HugeButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function onTap;
+
+  const HugeButton({
+    Key key,
+    @required this.color,
+    @required this.text,
+    @required this.icon,
+    @required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          borderRadius: BorderRadius.circular(1000),
+          onTap: onTap,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+                color: color, borderRadius: BorderRadius.circular(1000)),
+            child: Icon(
+              icon,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: 200,
+            ),
+          ),
+        ),
+        Text(
+          text,
+          style: Theme.of(context).textTheme.headline6,
+        )
+      ],
+    );
   }
 }
